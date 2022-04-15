bab0648069e5e5663ba02a0f09dd23c0b2b7c51b
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-15 20:57:19 -0300

    add form component

--- Desktop/formulario/src/app/app.module.ts
+++ Desktop/formulario/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
