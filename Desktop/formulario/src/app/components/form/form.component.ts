ed86db2a655425da396c7026bb30364a162c81a2
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-16 13:39:01 -0300

    add show/hide password

--- Desktop/formulario/src/app/components/form/form.component.ts
+++ Desktop/formulario/src/app/components/form/form.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  private passwordIsShow: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public setPasswordIsShow(): void {
+    this.passwordIsShow = !this.passwordIsShow;
   }
-
+  public getPasswordIsShow(): boolean {
+    return this.passwordIsShow;
+  }
 }
