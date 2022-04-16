1df434d9497883733809a5e5944b7a415b1a3e2a
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-04-16 13:44:21 -0300

    add form controls

--- Desktop/formulario/src/app/components/form/form.component.ts
+++ Desktop/formulario/src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit {
   private passwordIsShow: boolean = true;
   constructor() {}
-
+  public data = new FormGroup({
+    text: new FormControl({ value: '', disabled: true }, Validators.required),
+    password: new FormControl(
+      {
+        value: '',
+        disabled: true,
+      },
+      Validators.required
+    ),
+    email: new FormControl({ value: '', disabled: true }, [
+      Validators.required,
+      Validators.email,
+    ]),
+    number: new FormControl({ value: '', disabled: true }, Validators.required),
+    // checkbox: new FormArray([new FormControl()]),
+    date: new FormControl({ value: '', disabled: true }, Validators.required),
+    selection: new FormControl(
+      {
+        value: '',
+        disabled: true,
+      },
+      Validators.required
+    ),
+  });
   ngOnInit(): void {}
   public setPasswordIsShow(): void {
     this.passwordIsShow = !this.passwordIsShow;
